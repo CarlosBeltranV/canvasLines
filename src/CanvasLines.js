90c8ab8eaad1541ce3d4df034d865eb46122199d
cbeltranv <cbeltranv@cable.net.co>
2020-06-06 06:35:46 -0500

    Finalizado el componente con tramado más interesante

--- src/CanvasLines.js
+++ src/CanvasLines.js
@@ -4,22 +4,45 @@ export class CanvasLines extends LitElement {
 
   static get properties() {
     return {
-      d: { type: Object },
       lienzo: { type: Object }
     }
   }
 
   constructor() {
     super()
-    this.d = document.getElementById("dibujito")
-    this.lienzo = this.d.getContext("2d")
-    this.dibujarLinea("pink", 10, 300, 220, 10)
-    this.dibujarLinea("yellow", 300, 10, 10, 220)
+    this.esperar()
   }
 
   render() {
     return html`
+      <h2>Líneas en Canvas</h2>
+      <canvas width="300" height="300" id="dibujito"></canvas>
+      <p>Así quedan las líneas</p>
     `
+  }
+
+  async esperar() {
+    let listo = false
+    while(!listo) {
+      listo = await this.updateComplete
+    }
+    this.crearLienzo()
+  }
+
+  crearLienzo() {
+    let d = this.shadowRoot.getElementById("dibujito")
+    this.lienzo = d.getContext("2d")
+    let lineas = 30
+    let l = 0
+    let colorcito = "#FAA"
+    while(l < lineas) {
+      let yi = 10 * l
+      let xf = 10 * (l + 1)
+      this.dibujarLinea(colorcito, 0, yi, xf, 300)
+      l++
+    }
+    this.dibujarLinea(colorcito, 1, 1, 1, 299)
+    this.dibujarLinea(colorcito, 1, 299, 299, 299)
   }
 
   dibujarLinea(color, xinicial, yinicial, xfinal, yfinal) {
@@ -30,6 +53,5 @@ export class CanvasLines extends LitElement {
     this.lienzo.stroke();
     this.lienzo.closePath();
   }
-  
 
 }
